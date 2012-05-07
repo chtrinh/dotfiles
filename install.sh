d2fda63aa7312db119215ea3f9840386bba98a95
Jonathan Nevelson <jnevelson@gmail.com>
2012-05-06 21:36:40 -0700

    fix install script to work with directories

--- install.sh
+++ install.sh
@@ -6,8 +6,16 @@ dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 for file in *
 do
   if [[ $file != ".git" && $file != "README.md" && $file != "install.sh" && $file != ".gitmodules" ]]; then
-    echo "Symlinking $file..."
-    ln -s $dir/$file ~/$file
+    path=$dir/$file
+    if [[ -d $path ]]; then
+      if [[ ! -d ~/$file ]]; then
+        echo "Symlinking directory $path..."
+        ln -s $path ~/
+      fi
+    elif [[ ! -f ~/$file ]]; then
+      echo "Symlinking file $path..."
+      ln -s $path ~/
+    fi
   fi
 done;
 
