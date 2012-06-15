66de18270742cd7c1d1a0ce8741ea9c568ac8529
Jonathan Nevelson <jnevelson@gmail.com>
2012-06-15 10:13:17 -0700

    rename all dotfiles
    this way I don't have to ls -la to view them in the repo

--- install.sh
+++ install.sh
@@ -8,14 +8,15 @@ do
   # pretty lame... I need to work on my bash-fu
   if [[ $file != ".git" && $file != "README.md" && $file != "install.sh" && $file != ".gitmodules" && $file != ".gitignore" ]]; then
     path=$dir/$file
+    dotfile=.$file
     if [[ -d $path ]]; then
-      if [[ ! -d ~/$file ]]; then
+      if [[ ! -d ~/$dotfile ]]; then
         echo "Symlinking directory $path..."
-        ln -s $path ~/
+        ln -s $path ~/$dotfile
       fi
-    elif [[ ! -f ~/$file ]]; then
+    elif [[ ! -f ~/$dotfile ]]; then
       echo "Symlinking file $path..."
-      ln -s $path ~/
+      ln -s $path ~/$dotfile
     fi
   fi
 done;
